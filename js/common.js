841015456b1e8061e8534692891cd04f7803cde9
Dubovskiy Anton <megatoshik@inbox.ru>
2018-03-03 21:34:19 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,25 +1,5 @@
 
 
-//============================================================================================================ подключение карты google maps//
-
-
-//=========================================================================================== menu_open//
-
-window.onload = function () { //пусть страница грузанется
-    var mnu = document.querySelector(".toggle_mnu"); //выберем определенный узел по классу через queryselector
-    var hmenu = document.querySelector(".header__hidden-menu"); //cоздаем переменную для cпрятанного подменю чтоб отобразить его
-    var sandwich = document.querySelector(".sandwich"); //cоздаем переменную для cпрятанного подменю крестик!
-
-    //пишем для него событие
-
-    mnu.addEventListener('click', function () {
-        hmenu.classList.toggle("hidden_appear");
-        sandwich.classList.toggle("active");
-    });
-
-};
-
-
 jQuery(document).ready(function () {
 
     //================================================================================= mate_accord //
